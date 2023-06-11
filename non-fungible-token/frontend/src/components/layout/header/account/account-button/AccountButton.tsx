9d5b3ddfde7dd2404c18fc1d3624f7bd5316bad1
criptomty <59593000+riciluci@users.noreply.github.com>
2023-06-11 03:33:44 -0600

    gear
    Non fungible

---
+++ non-fungible-token/frontend/src/components/layout/header/account/account-button/AccountButton.tsx
@@ -0,0 +1,36 @@
+import Identicon from '@polkadot/react-identicon';
+import clsx from 'clsx';
+import { Mint } from './Mint';
+import { buttonStyles } from '@gear-js/ui';
+import { AllBalancesFungibleToken } from './AllBalancesFungibleToken';
+
+type Props = {
+  address: string;
+  name: string | undefined;
+  onClick: () => void;
+  isActive?: boolean;
+  block?: boolean;
+};
+
+function AccountButton({ address, name, onClick, isActive, block }: Props) {
+  const className = clsx(
+    buttonStyles.button,
+    buttonStyles.normal,
+    isActive ? buttonStyles.primary : buttonStyles.secondary,
+    block && buttonStyles.block,
+  );
+
+  return (
+    <>
+    <Mint/>   
+    <AllBalancesFungibleToken/>
+    
+    <button type="button" className={className} onClick={onClick}>
+      <Identicon value={address} className={buttonStyles.icon} theme="polkadot" size={28} />
+      {name}
+    </button>
+    </>
+  );
+}
+
+export { AccountButton };
